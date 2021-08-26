44602f03dfa28500788871b57132fa534ec8417d
Tom Rosen <55321560+TomRosen@users.noreply.github.com>
2021-08-26 12:36:33 +0200

    Added Navbar

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import Head from 'next/head';
 import '../styles/globals.css';
+import Link from 'next/link';
 
 function MyApp({ Component, pageProps }) {
 	return (
@@ -9,6 +10,50 @@ function MyApp({ Component, pageProps }) {
 			<Head>
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 			</Head>
+			<header>
+				<nav className='navbar navbar-expand-lg navbar-light bg-light'>
+					<div className='container-fluid'>
+						<Link href='/'>
+							<a className='navbar-brand'>NextPlanner</a>
+						</Link>
+
+						<button
+							className='navbar-toggler'
+							type='button'
+							data-bs-toggle='collapse'
+							data-bs-target='#navbarNavAltMarkup'
+							aria-controls='navbarNavAltMarkup'
+							aria-expanded='false'
+							aria-label='Toggle navigation'
+						>
+							<span className='navbar-toggler-icon'></span>
+						</button>
+						<div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
+							<div className='navbar-nav'>
+								<Link href='#'>
+									<a className='nav-link active' aria-current='page'>
+										Dashboard
+									</a>
+								</Link>
+								<Link href='/calendar'>
+									<a className='nav-link'>Planner</a>
+								</Link>
+								<Link href='#'>
+									<a className='nav-link'>Marks</a>
+								</Link>
+								<a
+									className='nav-link disabled'
+									href='#'
+									tabIndex='-1'
+									aria-disabled='true'
+								>
+									Placeholder
+								</a>
+							</div>
+						</div>
+					</div>
+				</nav>
+			</header>
 			<Component {...pageProps} />
 		</>
 	);
