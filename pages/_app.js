940ff77d91936b21e672200a3175496bb5ebffc1
Tom Rosen <55321560+TomRosen@users.noreply.github.com>
2021-08-30 09:32:50 +0200

    com

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 // add bootstrap css
 import 'bootstrap/dist/css/bootstrap.css';
 import Head from 'next/head';
+import Navbar from '@components/Navbar';
 import '../styles/globals.css';
 import Link from 'next/link';
 import { Provider } from 'next-auth/client';
@@ -12,46 +13,13 @@ function MyApp({ Component, pageProps }) {
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 			</Head>
 			<header>
-				<nav className='navbar navbar-expand-lg navbar-light bg-light'>
-					<div className='container-fluid'>
-						<Link href='/'>
-							<a className='navbar-brand'>NextPlanner</a>
-						</Link>
-
-						<button
-							className='navbar-toggler'
-							type='button'
-							data-bs-toggle='collapse'
-							data-bs-target='#navbarNavAltMarkup'
-							aria-controls='navbarNavAltMarkup'
-							aria-expanded='false'
-							aria-label='Toggle navigation'>
-							<span className='navbar-toggler-icon'></span>
-						</button>
-						<div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
-							<div className='navbar-nav'>
-								<Link href='/dashboard'>
-									<a className='nav-link active' aria-current='page'>
-										Dashboard
-									</a>
-								</Link>
-								<Link href='/calendar'>
-									<a className='nav-link'>Planner</a>
-								</Link>
-								<Link href='/grades'>
-									<a className='nav-link'>Grades</a>
-								</Link>
-								<a
-									className='nav-link disabled'
-									href='#'
-									tabIndex='-1'
-									aria-disabled='true'>
-									Placeholder
-								</a>
-							</div>
-						</div>
-					</div>
-				</nav>
+				<Navbar
+					navs={{
+						Dashboard: '/dashboard',
+						Planner: '/calendar',
+						Grades: '/grades',
+					}}
+				/>
 			</header>
 			<Component {...pageProps} />
 		</Provider>
